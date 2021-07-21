a80be9e8530e0f8d2852afdc01a057daa82e2671
James O'Keeffe <jamesokeeffe@jamess-mbp.home>
2021-07-21 18:04:25 +0800

    CSS applied to correctly center panels on page

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import SpotifyWebApi from 'spotify-web-api-node'
-import { Container } from 'react-bootstrap'
 import { useState, useEffect } from 'react'
 import useAuth from './useAuth'
 import Panel from './Panel'
@@ -100,13 +99,13 @@ export default function Dashboard({ code }) {
     
     return (
    
-    <Container>
+    <div>
       <Panel name='Top Artists' content={topArtists} />
       <Panel name='Recent' content={recent.slice(0, 5)} />
       <Panel name='More Like That Artist You Like' content={moreLike.slice(0, 5)} />
       <Panel name='Essential Somebody' content={essentialArtist.slice(0, 5)} />
       <Panel name='Recommended' content={recommend.slice(0, 5)} />
-    </Container>
+    </div>
    
     )
 }
--- client/src/Panel.js
+++ client/src/Panel.js
@@ -4,9 +4,9 @@ import { Container, Card } from 'react-bootstrap'
 export default function panel(props) {
      
     return (
-      <Container className='d-flex justify-content-space-between'>
+      <Container className='d-flex justify-content-between' style={{maxWidth: '1200px'}}>
       {props.content.map(cont =>
-        <Card style={{minWidth: '220px', height: '295px', margin: '10px', background: '#212121'}} key={cont.key}>
+        <Card style={{minWidth: '220px', height: '295px', marginTop: '10px', background: '#212121'}} key={cont.key}>
           <Card.Img style={{height: '190px', width: '190px', margin: '15px', objectFit: 'cover'}} src={cont.imgUrl} />
            <Card.Title style={{color: 'white', marginLeft: '15px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: '190px'}}>{cont.name}</Card.Title>
            <Card.Subtitle className="mb-2 text-muted" style={{color: 'white', marginLeft: '15px'}}>Content info</Card.Subtitle>
