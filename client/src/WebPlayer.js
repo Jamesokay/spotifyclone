ead24aceac9d65ef31b35d876f357a77c773db46
James OKeeffe <jamesokeeffe50@gmail.com>
2021-10-01 19:27:18 +0800

    Rough implementation of volume icon

--- client/src/WebPlayer.js
+++ client/src/WebPlayer.js
@@ -386,6 +386,20 @@ export default function WebPlayer() {
             </div>
       </div>
       <div className='playingTimeTotal'>{toMinsSecs(total)}</div>
+
+      <svg className='volumeIcon'
+           xmlns="http://www.w3.org/2000/svg" 
+           width="24" 
+           fill="none" 
+           height="24" 
+           viewBox="0 0 24 24" 
+           stroke="grey" 
+           strokeWidth="1" 
+           strokeLinecap="round" 
+           strokeLinejoin="round">
+            <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+            {vol > 50 ? (<path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>) : (<path d="M19.07 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>)}
+        </svg>
       <div id='volumeBar'
            onMouseOver={()=> setVolHover(true)} 
            onMouseLeave={()=> setVolHover(false)}  
