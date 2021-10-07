d90d3a3277e94932a8f083e5390f20043b8a4a22
James OKeeffe <jamesokeeffe50@gmail.com>
2021-10-07 09:29:44 +0800

    Basic set up of canvas API to read RGBA values of headerImage

--- client/src/HeaderPanel.js
+++ client/src/HeaderPanel.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 export default function HeaderPanel({ content, creators, dispatch }) {
 
   function pageChange(pageId) {
@@ -7,9 +9,32 @@ export default function HeaderPanel({ content, creators, dispatch }) {
       })
     }
 
+    const [bg, setBg] = useState('grey')
+    
+    function getData() {
+      var canvas = document.createElement('canvas');
+      var ctx    = canvas.getContext('2d');
+  
+      // 2) Copy your image data into the canvas
+      var myImgElement = document.getElementById('headerImage');
+      myImgElement.crossOrigin = ''
+      ctx.drawImage( myImgElement, 0, 0 );
+  
+      // 3) Read your image data
+     // var w = myImgElement.width, h=myImgElement.height;
+      var imgdata = ctx.getImageData(100,100,50,50);
+      var rgba = imgdata.data;
+      var colorRaw = rgba.slice(0,4)
+      setBg('rgba(' + colorRaw.join() + ')')
+      
+      
+    }
+
+
+
     return (
-        <div className='headerPanel'>
-          <img className='headerImage' src={content.imgUrl} alt=''/>
+        <div className='headerPanel' style={{background: bg}}>
+          <img  id='headerImage' src={content.imgUrl} alt='' onClick={()=> getData()}/>
             <div className='headerInfo'>
               <p className='headerType'>{content.type}</p>
               <p className='headerTitle'>{content.title}</p>
