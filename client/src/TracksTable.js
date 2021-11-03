9955561426a75524161f0d39f60afa11654c0b68
James OKeeffe <jamesokeeffe50@gmail.com>
2021-11-03 17:23:26 +0800

    Working implementation of IntersectionObserver CSS updates on TracksTable

--- client/src/TracksTable.js
+++ client/src/TracksTable.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from './AuthContext'
 import playTrack from './playTrack'
 
@@ -6,24 +6,35 @@ export default function TracksTable({content, page}) {
 
     const accessToken = useContext(AuthContext)
     const tableTop = document.getElementById('tableTop')
+//    const tableTopEdges = document.getElementsByClassName('empty')
+    const [scrolling, setScrolling] = useState(false)
 
 
 
       useEffect(() => {
         if (!tableTop) return
+ //       if (!tableTopEdges) return
 
         let options = {
           root: null,
-          rootMargin: '-8% 0% 0% 0%',
+          rootMargin: '-8.1% 0% 0% 0%',
           threshold: [0, 1]
         }
     
         function navTableHead(entries) {
           if (entries[0].isIntersecting) {
             tableTop.style.backgroundColor = 'transparent'
+            setScrolling(false)
+            // for (let i = 0; i < 2; i++) {
+            //   tableTopEdges[i].style.borderBottom = 'none'
+            // }
           }
           else {
-            tableTop.style.backgroundColor = 'red'
+            tableTop.style.backgroundColor = '#212121'
+            setScrolling(true)
+            // for (let i = 0; i < 2; i++) {
+            //   tableTopEdges[i].style.borderBottom = '1px solid rgb(105, 105, 105, 0.3)'
+            // }
           }
         }
      
@@ -109,11 +120,13 @@ export default function TracksTable({content, page}) {
             <table className='trackTable' cellSpacing='0' cellPadding='0'>
               <thead>
                 <tr id='tableTop'>
+                <th className='empty' style={(scrolling)? {borderBottom: '1px solid rgb(105, 105, 105, 0.3)'} : {borderBottom: 'none'}}></th>
                 <th style={{textAlign: 'center'}}>#</th>
                 <th>TITLE</th>
                 <th></th>
                 <th>ALBUM</th>
                 <th>TIME</th>
+                <th className='empty' style={(scrolling)? {borderBottom: '1px solid rgb(105, 105, 105, 0.3)'} : {borderBottom: 'none'}}></th>
                 </tr>
               </thead>
               <tbody>
@@ -122,6 +135,7 @@ export default function TracksTable({content, page}) {
               </tr>
               {content.map(cont =>
                 <tr className='trackTableRow' key={cont.id}>
+                <td className='emptyCell'></td>
                   <td className='rowFirst'>
                     <span className='tableIndex'>{cont.num}</span>
                     <div className='tablePlayIcon'
@@ -146,6 +160,7 @@ export default function TracksTable({content, page}) {
                   </td>
                   <td style={{width: '505px'}}><span className='tableLink'>{cont.albumName}</span></td>
                   <td className='rowLast'>{cont.duration}</td>
+                  <td className='emptyCell'></td>
                 </tr>
               )}
               </tbody>
