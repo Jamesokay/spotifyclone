5c1517c766333f047ef2929eb76506872dd59b46
James OKeeffe <jamesokeeffe50@gmail.com>
2022-01-01 23:31:48 +0800

    CSS fix for artist image on cards

--- client/src/components/Panel.js
+++ client/src/components/Panel.js
@@ -69,9 +69,7 @@ export default function Panel({ content, type }) {
                 onContextMenu={(e) => setRightClick({type: cont.type, yPos: e.screenY, xPos: e.screenX, id: cont.id})}>
           <div className='cardBody' style={(rightClick.id === cont.id)? {background: 'rgb(40, 40, 40)'} : {}}>
             {cont.type === 'artist'?
-            <div className='cardImageBox'>
             <img className='cardArtist' src={cont.imgUrl} alt='' />
-            </div>
             :
             <div className='cardImageBox'>
               <img className='cardImage' src={cont.imgUrl} alt='' />
